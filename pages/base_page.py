c4ebc2acd4cb9cdefa36b3980a0b293151c3c8e5
Platon T <platon@meloboss.ru>
2025-10-03 12:00:54 +0300

    класснор сделал

--- pages/base_page.py
+++ pages/base_page.py
@@ -8,3 +8,4 @@ class BasePage:
     def __init__(self, driver):
         self.driver = driver
         self.wait = WebDriverWait(driver, config.get('timeout'))
+        self.short_wait = WebDriverWait(driver, config.get('short_timeout'))
